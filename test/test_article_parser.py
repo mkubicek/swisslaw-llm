6bfe106f6040e4a9e4b5c8d849f8b5c228f8e297
mkubicek <milankubicek@hotmail.com>
2024-02-26 22:17:50 +0100

    add test for OR Art. 1 parsing

--- test/test_article_parser.py
+++ test/test_article_parser.py
@@ -31,6 +31,17 @@ class TestArticleParser(unittest.TestCase):
             articles = parse_articles(xml_data)
             for article in articles:
                 self.assertTrue(len(article.paragraphs) > 0, f"Article {article.article_number} in {law} does not contain any paragraphs.")
+                
+    def test_or_parse_article_1(self):
+        # read assets/OR_Art1.xml and parse it
+        with open('test/assets/OR_Art1.xml', 'r') as file:
+            xml_data = file.read()
+        articles = parse_articles(xml_data)
+        self.assertTrue(len(articles[0].paragraphs) == 2, "Article 1 in OR should contain 2 paragraph.")
+        self.assertTrue(len(articles[0].article_number) == 1, "Article 1 in OR should have the number 1.")
+        
+        article_1_paragraphs = ['Zum Abschlusse eines Vertrages ist die übereinstimmende gegenseitige Willensäusserung der Parteien erforderlich.', 'Sie kann eine ausdrückliche oder stillschweigende sein.']
+        self.assertEqual(articles[0].paragraphs, article_1_paragraphs, "Article 1 in OR does not contain the expected paragraphs.")
         
 if __name__ == '__main__':
     unittest.main()
